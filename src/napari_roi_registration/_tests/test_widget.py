7bc0577952c893dd81ae4a7bd08583a2a56cc8cc
GiorgiaTortora <giorgiatortora2@gmail.com>
2022-05-04 10:36:33 +0200

    test modified

--- src/napari_roi_registration/_tests/test_widget.py
+++ src/napari_roi_registration/_tests/test_widget.py
@@ -7,14 +7,14 @@ def test_subtract_background(make_napari_viewer, capsys):
 
     # background_widget = subtract_background()
     
-def test_register_rois(make_napari_viewer, capsys):
-    viewer = make_napari_viewer()
-    # layer = viewer.add_image(np.random.random((100, 100)))
+# def test_register_rois(make_napari_viewer, capsys):
+#     viewer = make_napari_viewer()
+#     # layer = viewer.add_image(np.random.random((100, 100)))
 
-    # register_widget = register_rois()
+#     # register_widget = register_rois()
     
-def test_process_rois(make_napari_viewer, capsys):
-    viewer = make_napari_viewer()
-    # layer = viewer.add_image(np.random.random((100, 100)))
+# def test_process_rois(make_napari_viewer, capsys):
+#     viewer = make_napari_viewer()
+#     # layer = viewer.add_image(np.random.random((100, 100)))
 
-    # processing_widget = process_rois()
+#     # processing_widget = process_rois()
