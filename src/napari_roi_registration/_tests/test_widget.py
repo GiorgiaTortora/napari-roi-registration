697e6765130156530660cab62f987808e8060a6e
GiorgiaTortora <giorgiatortora2@gmail.com>
2022-05-04 11:53:39 +0200

    test modified

--- src/napari_roi_registration/_tests/test_widget.py
+++ src/napari_roi_registration/_tests/test_widget.py
@@ -1,7 +1,7 @@
 from napari_roi_registration import register_rois
 
-# def test_register_rois(make_napari_viewer, capsys):
-#     viewer = make_napari_viewer()
+def test_register_rois(make_napari_viewer, capsys):
+    viewer = make_napari_viewer()
     # layer = viewer.add_image(np.random.random((100, 100)))
 
     # register_widget = register_rois()
